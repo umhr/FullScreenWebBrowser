6baa1c86b95b5966596f3eba4c5bb62ccd4390e0
Hiroshi Umehara <com@umehara.net>
2014-08-24 13:06:23 +0900

    アプリにhtmlファイルをドロップして開くのに対応

--- FullScreenWebBrowser/FullScreenWebBrowser/Form1.cs
+++ FullScreenWebBrowser/FullScreenWebBrowser/Form1.cs
@@ -46,6 +46,23 @@ namespace FullScreenWebBrowser
 
             // バイナリーと同階層のindex.htmlをブラウザで表示
             String htmlPath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "index.html";
+
+            // 実行ファイルにファイルをドロップした場合はそのパスに上書き
+            // 実行ファイルにドロップされたファイルのパスを取得する
+            // http://dobon.net/vb/dotnet/programing/dropfiletoexe.html
+            string[] cmds = System.Environment.GetCommandLineArgs();
+
+            if (cmds.Length > 1)
+            {
+                //ドロップされたファイルのパスをすべて表示
+                for (int i = 1; i < cmds.Length; i++)
+                {
+                    // Console.WriteLine(cmds[i]);
+                    htmlPath = cmds[i];
+                }
+            }
+
+            // ブラウザにURIを渡す。
             webBrowser1.Url = new Uri(htmlPath);
         }
     }
